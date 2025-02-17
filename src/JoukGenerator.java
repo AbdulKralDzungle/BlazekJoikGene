62909c6395aaccac80f19f202f084032aeab1918
damia <dambibimbus@gmail.com>
2025-02-17 18:53:11 +0100

    smth

--- src/JoukGenerator.java
+++ src/JoukGenerator.java
@@ -10,11 +10,67 @@ public class JoukGenerator {
     public String newJoke() {
         String s = "";
         for (int i = 0; i < rn.nextInt(10); i++) {
-            s += switch (rn.nextInt(5)) {
-                default -> podsJm[rn.nextInt(podsJm.length)] +
-                        slovesa[rn.nextInt(slovesa.length)] +
-                        spojky[rn.nextInt(spojky.length)] +
+
+            s += switch (rn.nextInt(6)) {
+                default -> podsJm[rn.nextInt(podsJm.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
                         podsJm[rn.nextInt(podsJm.length)];
+                case 0 -> podsJm[rn.nextInt(podsJm.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " ";
+                case 1 -> podsJm[rn.nextInt(podsJm.length)];
+                case 2 -> podsJm[rn.nextInt(podsJm.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " ";
+                case 3 -> přJm[rn.nextInt(přJm.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " ";
+                case 4 -> přJm[rn.nextInt(přJm.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " ";
+                case 5 -> přJm[rn.nextInt(přJm.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        slovesa[rn.nextInt(slovesa.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        spojky[rn.nextInt(spojky.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + " ";
             };
         }
         return s;
