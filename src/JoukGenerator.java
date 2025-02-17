d7426489150b9093f01b6db1cae1e3cfeada7adc
damia <dambibimbus@gmail.com>
2025-02-17 20:10:10 +0100

    smth

--- src/JoukGenerator.java
+++ src/JoukGenerator.java
@@ -1,15 +1,15 @@
 import java.util.Random;
 
 public class JoukGenerator {
-    Random rn = new Random();
-    String[] podsJm = new String[]{"flašky", "stole", "stůl", "LED dioda", "ANYONY", "anion", "rašek", "péro", "spikiball", "teta", "tety", "babička", "nábitek", "flammenwerfer", "noha", "ledvina", "ichtil", "ABDUL", "Démosténes Kreténes", "král", "džungle", "černoch"};
-    String[] slovesa = new String[]{"sedí", "je", "chodí", "jí", "stojí"};
-    String[] spojky = new String[]{"na", "pod", "v", "mozi", "a", "velde", "nad", "blízko", "s", "u",};
-    String[] přJm = new String[]{"velmi", "1,5", "černý", "humorný", "svítivý", "sigma", "Led", "tlustý", "málo", "holohlavé",};
+    private Random rn = new Random();
+    private String[] podsJm = new String[]{"flašky", "stole", "stůl", "LED dioda", "ANYONY", "anion", "rašek", "péro", "spikiball", "teta", "tety", "babička", "nábitek", "flammenwerfer", "noha", "ledvina", "ichtil", "ABDUL", "Démosténes Kreténes", "král", "džungle", "černoch"};
+    private String[] slovesa = new String[]{"sedí", "je", "chodí", "jí", "stojí"};
+    private String[] spojky = new String[]{"na", "pod", "v", "mozi", "a", "velde", "nad", "blízko", "s", "u",};
+    private String[] přJm = new String[]{"velmi", "1,5", "103,5", "černý", "humorný", "svítivý", "sigma", "Led", "tlustý", "málo", "holohlavé",};
 
     public String newJoke() {
         String s = "";
-        for (int i = 0; i < rn.nextInt(10); i++) {
+        for (int i = 0; i < 9999; i++) {
 
             s += switch (rn.nextInt(6)) {
                 default -> podsJm[rn.nextInt(podsJm.length)] + " " +
@@ -19,20 +19,20 @@ public class JoukGenerator {
                 case 0 -> podsJm[rn.nextInt(podsJm.length)] + " " +
                         slovesa[rn.nextInt(slovesa.length)] + " " +
                         spojky[rn.nextInt(spojky.length)] + " " +
-                        podsJm[rn.nextInt(podsJm.length)] + " ";
-                case 1 -> podsJm[rn.nextInt(podsJm.length)];
+                        podsJm[rn.nextInt(podsJm.length)] + ".\n ";
+                case 1 -> podsJm[rn.nextInt(podsJm.length)] + ".\n ";
                 case 2 -> podsJm[rn.nextInt(podsJm.length)] + " " +
                         slovesa[rn.nextInt(slovesa.length)] + " " +
                         přJm[rn.nextInt(přJm.length)] + " " +
                         spojky[rn.nextInt(spojky.length)] + " " +
-                        podsJm[rn.nextInt(podsJm.length)] + " ";
+                        podsJm[rn.nextInt(podsJm.length)] + ".\n ";
                 case 3 -> přJm[rn.nextInt(přJm.length)] + " " +
                         podsJm[rn.nextInt(podsJm.length)] + " " +
                         slovesa[rn.nextInt(slovesa.length)] + " " +
                         přJm[rn.nextInt(přJm.length)] + " " +
                         spojky[rn.nextInt(spojky.length)] + " " +
-                        podsJm[rn.nextInt(podsJm.length)] + " " +
-                        přJm[rn.nextInt(přJm.length)] + " ";
+                        přJm[rn.nextInt(přJm.length)] + " " +
+                        podsJm[rn.nextInt(podsJm.length)] + ".\n ";
                 case 4 -> přJm[rn.nextInt(přJm.length)] + " " +
                         podsJm[rn.nextInt(podsJm.length)] + " " +
                         slovesa[rn.nextInt(slovesa.length)] + " " +
@@ -43,7 +43,7 @@ public class JoukGenerator {
                         slovesa[rn.nextInt(slovesa.length)] + " " +
                         přJm[rn.nextInt(přJm.length)] + " " +
                         spojky[rn.nextInt(spojky.length)] + " " +
-                        podsJm[rn.nextInt(podsJm.length)] + " ";
+                        podsJm[rn.nextInt(podsJm.length)] + ".\n ";
                 case 5 -> přJm[rn.nextInt(přJm.length)] + " " +
                         podsJm[rn.nextInt(podsJm.length)] + " " +
                         slovesa[rn.nextInt(slovesa.length)] + " " +
@@ -70,7 +70,7 @@ public class JoukGenerator {
                         přJm[rn.nextInt(přJm.length)] + " " +
                         přJm[rn.nextInt(přJm.length)] + " " +
                         spojky[rn.nextInt(spojky.length)] + " " +
-                        podsJm[rn.nextInt(podsJm.length)] + " ";
+                        podsJm[rn.nextInt(podsJm.length)] + ".\n ";
             };
         }
         return s;
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+     JoukGenerator jg = new JoukGenerator();
+        System.out.println(jg.newJoke());
     }
 }
